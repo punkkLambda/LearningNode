758f9bc4c2bc9f26380687276de952cb459852b5
Xander Murray <xm720870@gmail.com>
2025-06-03 18:03:20 -0500

    commit

--- app.js
+++ app.js
@@ -26,5 +26,5 @@ app.get('/about-us', (req, res) => {
 // 404 page
 
 app.use((req, res) => {
-    res.sendFile('./views/404.html', { root: __dirname}) 
+    res.status(404).sendFile('./views/404.html', { root: __dirname}) 
 }); 
